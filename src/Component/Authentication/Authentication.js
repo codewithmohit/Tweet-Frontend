dde2e3650c24c292e0e3b00991621b194e279946
codewithmohit <jindalmohit2018@gmail.com>
2022-09-04 16:39:51 +0530

    Frontend

--- src/Component/Authentication/Authentication.js
+++ src/Component/Authentication/Authentication.js
@@ -6,7 +6,7 @@ function Authentication() {
     const url = "https://cors-everywhere.herokuapp.com/http://tweet-env.eba-34tepn6j.us-east-1.elasticbeanstalk.com/api/v1.0/tweets/";
     const user = localStorage.getItem("loginId")
 
-    axios.get(url + 'login/' + user)
+    axios.get(url + 'check/' + user)
         .then((e) => {
             console.log("Authentication == " + e.data);
             checkUser =  e.data;
